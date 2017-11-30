6c4ac8869ce6670131642379155ff6903bd6f313
TB831 <druu831@gmail.com>
2017-11-29 22:25:20 -0800

    added Entry to models.py

--- Journal/models.py
+++ Journal/models.py
@@ -6,8 +6,22 @@ from django.db import models
 # Create your models here.
 
 class Topic(models.Model):
+    #adds a topic
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.text
+
+class Entry(models.Model):
+    #adds a text entry
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        #return string representation of the model
+        return self.text[:50] + "..."
