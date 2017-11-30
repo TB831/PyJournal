aadd3d9dc9e751d50d0a51316669ac7618d6d022
TB831 <druu831@gmail.com>
2017-11-29 23:00:08 -0800

    added index.html template

--- PyJournal/settings.py
+++ PyJournal/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'PyJournal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'Journal/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
