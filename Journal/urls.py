c38e9e774b2149d70917d6f87d06124dcb39b01c
TB831 <druu831@gmail.com>
2017-11-29 23:41:31 -0800

    added single topic.html and its various entries

--- Journal/urls.py
+++ Journal/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     #Topics
-    url(r'^topics/$', views.topics, name='topics')
+    url(r'^topics/$', views.topics, name='topics'),
+
+    #Details for single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
 ]
--- Journal/views.py
+++ Journal/views.py
@@ -10,7 +10,15 @@ def index(request):
     return render(request, 'index.html')
 
 def topics(request):
+    #Shows different topics
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'topics.html', context)
+
+def topic(request, topic_id):
+    #Shows a single topic and its entries
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries': entries}
+    return render(request, 'topic.html', context)
 
