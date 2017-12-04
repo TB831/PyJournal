5367006c55b036bc0ff01da2968fb71c57473cf5
TB831 <druu831@gmail.com>
2017-12-04 13:35:48 -0800

    currently logout is setup, but logout isn't functioning

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,9 @@ from django.contrib.auth.views import login
 from . import views
 
 urlpatterns = [
+    #URL config for logging in
     url('^login/$', login, {'template_name':'login.html'}, name='login'),
+    #URL config for logging out
+    url(r'^logout/$', views.logout, name='logout'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+#Import for Django based logout module
+from django.contrib.auth import logout
+
 # Create your views here.
+def logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('Journal:index'))
