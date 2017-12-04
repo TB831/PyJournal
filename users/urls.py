6789e66076422d61d4b3258b00d77084f2a07006
TB831 <druu831@gmail.com>
2017-12-04 12:54:59 -0800

    fixed issue with form not showing in login.html

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth.views import login
 from . import views
 
 urlpatterns = [
-    url('^login/$', login, {'template_name':'users/login.html'}, name='login'),
+    url('^login/$', login, {'template_name':'login.html'}, name='login'),
 ]
