44bb2a26fad60e7326cdf326ce75c85b3b77f30f
TB831 <druu831@gmail.com>
2017-12-09 19:18:30 -0800

    added configs for image in forms and models

--- Journal/forms.py
+++ Journal/forms.py
@@ -10,7 +10,7 @@ class TopicForm(forms.ModelForm):
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
-        fields = ['text']
+        fields = ['text', 'image']
         labels = {'text': ''}
         # Django widget that will gives a sizeable writing area
         widgets = {'text': forms.Textarea(attrs={'cols':80})}
